d24343863e412e1f2db7bb4f2e0e9b7aed62817a
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-06 11:23:32 +0200

    kommentar

---
+++ src/sprachkern/Kommentare.java
@@ -0,0 +1,15 @@
+package sprachkern;
+
+public class Kommentare {
+    public static void main(String[]args) {
+
+        /*
+        mehrzeiliger
+        Kommentar
+        (darf nicht verschachtelt werden)
+        */
+
+
+        // einzeiliger Kommentar (bis Ende der Zeile)
+    }
+}

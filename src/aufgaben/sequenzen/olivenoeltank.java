5913a7b4860de243140a75a8db08d4dcf6e13373
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-08 16:20:57 +0200

    new files

--- src/aufgaben/sequenzen/olivenoeltank.java
+++ src/aufgaben/sequenzen/olivenoeltank.java
@@ -13,10 +13,18 @@ public class olivenoeltank {
     public static void main(String[] args) {
 
         // Eingabe
+        double tankSizeWidth = 2.5 * 100 ; // Meter in Centimeter
+        double dichteOlivenOel = 0.91; // cm³
+        double fuellMenge = 240;// cm (2400mm)
 
         // Verarbeitung
+        double volumen = Math.PI * ((tankSizeWidth / 2) * (tankSizeWidth / 2)) * fuellMenge ; // = 2.3561944901923448E7m³
+        double dichteMenge = volumen * dichteOlivenOel / 1000;
+        double literMenge = volumen * 0.001;
 
         // Ausgabe
+        System.out.printf("Füllmenge vom Olivenöl in Liter: %,.3f l \n ", literMenge);
+        System.out.printf("Füllmenge vom Olivenöl in Kilogramm: %,.3f kg \n", dichteMenge);
 
     }
 }

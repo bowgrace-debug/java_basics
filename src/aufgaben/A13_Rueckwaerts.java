959f90e8c44faf09df9beb58abaf8ed13f417a57
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-15 09:03:44 +0200

    add A13 und A14

---
+++ src/aufgaben/A13_Rueckwaerts.java
@@ -0,0 +1,47 @@
+package aufgaben;
+
+/**
+ * A13_Rueckwaerts
+ * <p>
+ *
+ * @author Maximilian Stolle
+ * @version 14.08.2024
+ */
+public class A13_Rueckwaerts {
+    public static void main(String[] args) {
+
+        String text = "Ein beliebiger Text";
+        int laenge = text.length();
+        int i = laenge -1;
+
+        while(i >= 0) {
+            System.out.println(text.charAt(i));
+            i--;
+        }
+
+        System.out.println();
+        System.out.println("========================");
+        System.out.println();
+
+        String name = "Maximilian Stolle";
+        int stellenAlsZahl = name.length();
+        int index = 0;;
+        while(index < 17) {
+            System.out.println(name.charAt(index));
+            index++;
+        }
+
+        System.out.println();
+        System.out.println("========================");
+        System.out.println();
+
+        String fullname = "Maximilian Stolle";
+        int laengeString = fullname.length();
+        int stelleString = laengeString -1;
+
+        while (stelleString >= 0) {
+            System.out.println(fullname.charAt(stelleString));
+            stelleString--;
+        }
+    }
+}
---
+++ src/aufgaben/A14_UmlauteZaehlen.java
@@ -0,0 +1,76 @@
+package aufgaben;
+
+/**
+ * A14_UmlauteZaehlen
+ * <p>
+ *
+ * @author Maximilian Stolle
+ * @version 14.08.2024
+ */
+public class A14_UmlauteZaehlen {
+    public static void main(String[] args) {
+
+        // Lösung 1
+
+        String text = "Übel wütet der kahlköpfige Gürtelwürger";
+        text = text.toLowerCase();
+        int laenge = text.length();
+        int i = 0;
+        int counterAe = 0;
+        int counterOe = 0;
+        int counterUe = 0;
+
+        while (i < laenge) {
+            if (text.charAt(i) == 'ä') {
+                counterAe += 1;
+            }
+            else {
+                if (text.charAt(i) == 'ö') {
+                    counterOe += 1;
+                }
+                else {
+                    if (text.charAt(i) == 'ü') {
+                        counterUe += 1;
+                    }
+                }
+            }
+            i++;
+        }
+        System.out.println("ä kommt " + counterAe + " mal vor");
+        System.out.println("ö kommt " + counterOe + " mal vor");
+        System.out.println("ü kommt " + counterUe + " mal vor");
+
+
+        System.out.println();
+        System.out.println("========================");
+        System.out.println();
+
+
+        // Lösung 2
+
+        String text1 = "Übel wütet der kahlköpfige Gürtelwürger".toLowerCase();
+        laenge = text1.length();
+         i = 0;
+         counterAe = 0;
+         counterOe = 0;
+         counterUe = 0;
+
+         while (i < laenge) {
+             switch (text.charAt(i)) {
+                 case ('ä'):
+                     counterAe++;
+                     break;
+                 case ('ö'):
+                     counterOe++;
+                     break;
+                 case ('ü'):
+                     counterUe++;
+                     break;
+             }
+             i++;
+         }
+        System.out.println("ä kommt " + counterAe + " mal vor");
+        System.out.println("ö kommt " + counterOe + " mal vor");
+        System.out.println("ü kommt " + counterUe + " mal vor");
+    }
+}

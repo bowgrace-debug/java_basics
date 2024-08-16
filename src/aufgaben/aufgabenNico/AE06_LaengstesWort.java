485aa187bf32e1ca5d11f61a9cb812ed8db679d0
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-16 09:13:18 +0200

    add

--- src/aufgaben/aufgabenNico/AE06_LaengstesWort.java
+++ src/aufgaben/aufgabenNico/AE06_LaengstesWort.java
@@ -10,10 +10,25 @@ package aufgaben.aufgabenNico;
 public class AE06_LaengstesWort {
     public static void main(String[] args) {
 
+
+        // Gebe das längste Wort aus dem Satz aus
+        /*
+            - Ich schaue zuerst nach dem Leerzeichen
+            - Zählen von Leerzeichen zu beginn des ersten Wortes
+            - brauche einen counter, welchen meinen höchsten Wert speichert
+            - Dann schauen wo das Leerzeichen ist und nehme den index -1 und -1 var lastword
+            - Dann überprüfen ob der tempCounter größer als der counter (Highscore)
+            - Dann setzte ich das lastword zu index vom letzten zu wissenden space (lastword = i)
+            - Dann gebe counter in der Konsole aus
+        */
+
         String txt = "Das ist ein Beispieltext";
+
+
         int laenge = txt.length();
         int i = 0;
         int counter = 0;
+        int tempCounter = 0;
         int lastword = 0;
         int nextword = 0;
 

93df4650cb962eb7697d0b04f181655de2dd6961
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-19 11:02:27 +0200

    add

---
+++ src/aufgaben/A19_SatzPalindrome.java
@@ -0,0 +1,14 @@
+package aufgaben;
+
+/**
+ * A19_SatzPalindrome
+ * <p>
+ *
+ * @author Maximilian Stolle
+ * @version 19.08.2024
+ */
+public class A19_SatzPalindrome {
+    public static void main(String[] args) {
+
+    }
+}

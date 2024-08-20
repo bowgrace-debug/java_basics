0aade526965caf9da7521aeee3e180180bec33f9
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-20 15:57:23 +0200

    change

--- src/aufgaben/A21_Arrays.java
+++ src/aufgaben/A21_Arrays.java
@@ -1,5 +1,8 @@
 package aufgaben;
 
+import java.util.Arrays;
+import java.util.IllegalFormatCodePointException;
+
 /**
  * A21_Arrays
  * <p>
@@ -9,10 +12,58 @@ package aufgaben;
  */
 public class A21_Arrays {
     public static void main(String[] args) {
-        int[] liste = {11,22,33,44,55,66,77,88,99,0};
+
+        // Aufgabe 1
+
+        int[] liste = {11, 22, 33, 44, 55, 66, 77, 88, 99, 0};
+        int zaehler = 0;
 
         for (int i = 0; i < liste.length; i++) {
-            System.out.println(liste[i]);
+            zaehler += liste[i];
+        }
+        System.out.print(zaehler);
+
+        System.out.println();
+        System.out.println("====================");
+
+        zaehler = 0;
+        for (int i : liste) {
+            zaehler += i;
+        }
+        System.out.println(zaehler);
+
+        System.out.println();
+        System.out.println("====================");
+
+        // Aufgabe 2
+
+        int min = liste[0];
+        int max = 0;
+        int i = 0;
+
+        for (i = 0; i < liste.length; i++) {
+            if (liste[i] <= min) {
+                min = liste[i];
+            }
+            if (liste[i] >= max) {
+                max = liste[i];
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
+
+        System.out.println();
+        System.out.println("====================");
+
+        // Aufgabe 3
+        String suche = "Sonntag";
+
+        String[] wochentage = {"Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"};
+
+        for (String finder : wochentage) {
+            if (finder == suche) {
+                System.out.println(finder);
+            }
         }
     }
 }

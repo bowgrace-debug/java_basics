fbee05d24aefdf9c651aa93859ea31cdaf5598ba
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-26 13:15:38 +0200

    add

--- src/aufgaben/A26_Tabellenkalkulation.java
+++ src/aufgaben/A26_Tabellenkalkulation.java
@@ -12,23 +12,23 @@ import java.util.Arrays;
 public class A26_Tabellenkalkulation {
     public static void main(String[] args) {
 
-        double[][] doubleFeld = {
+        double[][] tabelle = {
                 {12.50, 7.30, 5.20},
                 {10.80, 6.70, 19.40},
                 {120.00, 0.90, 2.15}
         };
 
         // for Schleife: gibt alles auf einmal aus
-        for (int i = 0; i < doubleFeld.length; i++) {
-            for (int x = 0; x < doubleFeld.length; x++) {
-                System.out.printf("%6.2f ", doubleFeld[i][x]);
+        for (int i = 0; i < tabelle.length; i++) {
+            for (int x = 0; x < tabelle.length; x++) {
+                System.out.printf("%6.2f ", tabelle[i][x]);
             }
         }
         System.out.println();
         System.out.println("==========================================");
 
         // erweiterte for Schleife: gibt alles einzeln aus
-        for (double[] zeile : doubleFeld) {
+        for (double[] zeile : tabelle) {
             for (double wert : zeile) {
                 System.out.printf("%6.2f ", wert);
             }
@@ -39,7 +39,7 @@ public class A26_Tabellenkalkulation {
         System.out.println("==========================================");
 
         // erweiterte for Schleife: gibt alles einzeln aus
-        for (double[] zeile : doubleFeld) {
+        for (double[] zeile : tabelle) {
             double summe = 0;
             for (double wert : zeile) {
                 summe += wert;
@@ -52,7 +52,7 @@ public class A26_Tabellenkalkulation {
         System.out.println();
         System.out.println("==========================================");
 
-        for (double[] zeile : doubleFeld) {
+        for (double[] zeile : tabelle) {
             double summe = 0;
             for (double wert : zeile) {
                 summe += wert;
@@ -62,9 +62,9 @@ public class A26_Tabellenkalkulation {
             System.out.println();
         }
         double [] spalten = {
-                doubleFeld[0][0] + doubleFeld[1][0] + doubleFeld[2][0],
-                doubleFeld[0][1] + doubleFeld[1][1] + doubleFeld[2][1],
-                doubleFeld[0][2] + doubleFeld[1][2] + doubleFeld[2][2]
+                tabelle[0][0] + tabelle[1][0] + tabelle[2][0],
+                tabelle[0][1] + tabelle[1][1] + tabelle[2][1],
+                tabelle[0][2] + tabelle[1][2] + tabelle[2][2]
         };
         System.out.printf(" %6.2f %6.2f %6.2f", spalten[0], spalten[1], spalten[2]);
 

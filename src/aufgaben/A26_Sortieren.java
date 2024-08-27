af3af03a063a7280a8db7739bf0a945ba82b3df0
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-27 09:35:09 +0200

    change files

--- src/aufgaben/A26_Sortieren.java
+++ src/aufgaben/A26_Sortieren.java
@@ -1,5 +1,7 @@
 package aufgaben;
 
+import java.util.Arrays;
+
 /**
  * A26_Sortieren
  * <p>
@@ -9,6 +11,29 @@ package aufgaben;
  */
 public class A26_Sortieren {
     public static void main(String[] args) {
+        int[] liste = {55, 7, 78, 12, 42};
+//        int[] liste = {};
+//        double[] liste = {123.45, 0, -2, 4, 4.04};
 
+        System.out.println("unsortiert:");
+        System.out.println(Arrays.toString(liste));
+
+        // Sortierung durch den Bubblesort-Algorithmus
+        boolean getauscht;
+        do {
+            getauscht = false;
+            for (int i = 0; i < liste.length -1; i++) {
+                if (liste[i] > liste[i + 1]) {
+                    int temp = liste[i];
+                    liste[i] = liste[i + 1];
+                    liste[i + 1] = temp;
+                    getauscht = true;
+                }
+            }
+        } while (getauscht);
+
+        // Ausgabe des sortierten Arrays
+        System.out.println("sortiert:");
+        System.out.println(Arrays.toString(liste));
     }
 }

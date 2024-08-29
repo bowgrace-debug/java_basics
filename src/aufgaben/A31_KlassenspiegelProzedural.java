a9d2615f4564bfc2ccd8f387a9b469df29cac5b8
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-08-29 09:28:58 +0200

    chnages

--- src/aufgaben/A31_KlassenspiegelProzedural.java
+++ src/aufgaben/A31_KlassenspiegelProzedural.java
@@ -19,26 +19,26 @@ public class A31_KlassenspiegelProzedural {
 
 
     // Berechnet die Note anhand der Eingabe (Prozentzahl)
-    static int berechnungNoten (String[] bewertung, int[] ergebnisse, double prozentzahl) {
+    static int berechnungNoten (double prozentzahl, int[] ergebnisse) {
         int note = 0;
 
-        if (prozentzahl >= 92) {
+        if (prozentzahl >= ergebnisse[0]) {
             note = 1;
         }
         else {
-            if (prozentzahl >= 81 && prozentzahl <= 91) {
+            if (prozentzahl <= ergebnisse[1] && prozentzahl >= ergebnisse[2]) {
                 note = 2;
             }
             else {
-                if (prozentzahl >= 67 && prozentzahl <= 80) {
+                if (prozentzahl <= ergebnisse[3] && prozentzahl >= ergebnisse[4]) {
                     note = 3;
                 }
                 else {
-                    if (prozentzahl >= 50 && prozentzahl <= 66) {
+                    if (prozentzahl <= ergebnisse[5] && prozentzahl >= ergebnisse[6]) {
                         note = 4;
                     }
                     else {
-                        if (prozentzahl >= 30 && prozentzahl <= 49) {
+                        if (prozentzahl <= ergebnisse[7] && prozentzahl >= ergebnisse[8]) {
                             note = 5;
                         }
                         else {
@@ -54,26 +54,26 @@ public class A31_KlassenspiegelProzedural {
 
 
     // Berechnet die Bewertung anhand der Eingabe (Prozentzahl)
-    static String berechnungBewertung (String[] bewertung, int[] ergebnisse, double prozentzahl) {
+    static String berechnungBewertung (String[] bewertung, double prozentzahl, int[] ergebnisse) {
         String wertung = "";
 
-            if (prozentzahl >= 92) {
+            if (prozentzahl >= ergebnisse[0]) {
                 wertung = bewertung[0];
             }
             else {
-                if (prozentzahl >= 81 && prozentzahl <= 91) {
+                if (prozentzahl <= ergebnisse[1] && prozentzahl >= ergebnisse[2]) {
                     wertung = bewertung[1];
                 }
                 else {
-                    if (prozentzahl >= 67 && prozentzahl <= 80) {
+                    if (prozentzahl <= ergebnisse[3] && prozentzahl >= ergebnisse[4]) {
                         wertung = bewertung[2];
                     }
                     else {
-                        if (prozentzahl >= 50 && prozentzahl <= 66) {
+                        if (prozentzahl <= ergebnisse[5] && prozentzahl >= ergebnisse[6]) {
                             wertung = bewertung[3];
                         }
                         else {
-                            if (prozentzahl >= 30 && prozentzahl <= 49) {
+                            if (prozentzahl <= ergebnisse[7] && prozentzahl >= ergebnisse[8]) {
                                 wertung = bewertung[4];
                             }
                             else {
@@ -88,31 +88,32 @@ public class A31_KlassenspiegelProzedural {
     }
 
     // Berechnet sowohl die Note, als auch die Bewertung anhand der Eingabe (Prozentzahl)
-    static int notenUndBewertungBerechnung (int[] ergebnisse, String[] bewertung, double prozentzahl) {
+    static int notenUndBewertungBerechnung (String[] bewertung, double prozentzahl, int[] ergebnisse) {
         int note = 0;
         String wertung = "";
 
-        if (prozentzahl >= 92) {
+        if (prozentzahl >= ergebnisse[0]) {
             note = 1;
             wertung = bewertung[0];
         }
         else {
-            if (prozentzahl >= 81 && prozentzahl <= 91) {
+            if (prozentzahl <= ergebnisse[1] && prozentzahl >= ergebnisse[2]) {
                 note = 2;
                 wertung = bewertung[1];
 
             }
             else {
-                if (prozentzahl >= 67 && prozentzahl <= 80) {
+                if (prozentzahl <= ergebnisse[3] && prozentzahl >= ergebnisse[4]) {
                     note = 3;
                     wertung = bewertung[2];
-                } else {
-                    if (prozentzahl >= 50 && prozentzahl <= 66) {
+                }
+                else {
+                    if (prozentzahl <= ergebnisse[5] && prozentzahl >= ergebnisse[6]) {
                         note = 4;
                         wertung = bewertung[3];
                     }
                     else {
-                        if (prozentzahl >= 30 && prozentzahl <= 49) {
+                        if (prozentzahl <= ergebnisse[7] && prozentzahl >= ergebnisse[8]) {
                             note = 5;
                             wertung = bewertung[4];
                         }
@@ -124,29 +125,29 @@ public class A31_KlassenspiegelProzedural {
                 }
             }
         }
-        System.out.printf("Anhand der erreichenden Prozentzahl von: %.1f %%,\nhaben Sie die Note: %d erreicht.\nDas entspricht der Bewertung: %s!\n",prozentzahl, note, wertung );
+        System.out.printf("Anhand der erreichten Prozentzahl von: %.1f %%,\nhaben Sie die Note: %d erreicht.\nDas entspricht der Bewertung: %s!\n",prozentzahl, note, wertung );
         return note;
     }
 
 
     public static void main(String[] args) {
 
-        int[] ergebnisse = {92, 81, 67, 50, 30, 0};
+        int[] ergebnisse = {92, 91, 81, 80, 67, 66, 50, 49, 30, 0};
         String[] bewertung = {"sehr gut", "gut", "befriedigend", "ausreichend", "mangelhaft", "ungenügend"};
 
         double prozentzahl = eingabeProzentzahl();
         System.out.println("=============================================");
 
         System.out.println("Aufgabe 1: \n");
-        int note = berechnungNoten(bewertung, ergebnisse, prozentzahl);
+        int note = berechnungNoten(prozentzahl, ergebnisse);
         System.out.println("=============================================");
 
         System.out.println("Aufgabe 2: \n");
-        String wertung = berechnungBewertung(bewertung, ergebnisse, prozentzahl);
+        String wertung = berechnungBewertung(bewertung,prozentzahl, ergebnisse);
         System.out.println("=============================================");
 
         System.out.println("Aufgabe 1 und 2 zusammen: \n");
-        int gesamteBewertung = notenUndBewertungBerechnung(ergebnisse, bewertung, prozentzahl);
+        int gesamteBewertung = notenUndBewertungBerechnung(bewertung, prozentzahl, ergebnisse);
 
 
     }

bef625bd8de25a6846778a25d45478f997c49d5c
Maximilian Stolle <146789736+bowgrace-debug@users.noreply.github.com>
2024-09-12 10:22:47 +0200

    changes

--- src/aufgaben/aufgaben_wiederholungen/eventCards/Eventcard.java
+++ src/aufgaben/aufgaben_wiederholungen/eventCards/Eventcard.java
@@ -10,10 +10,10 @@ package aufgaben.aufgaben_wiederholungen.eventCards;
  */
 public class Eventcard {
 
-    public static final String DATE_PATTERN = "([0-2][0-9]|[3][0-2])[.]([0][0-9]|[1][1-2])[.]([0-9]{4})";
-    public static final String TIME_PATTERN = "(([0-1][0-9]|[2][0-3]):([0-5][0-9]))";
+    public static final String DATE_PATTERN = "([0-2][0-9]|3[0-2])[.](0[0-9]|1[1-2])[.]([0-9]{4})";
+    public static final String TIME_PATTERN = "(([0-1][0-9]|2[0-3]).([0-5][0-9]))";
     public static final String PRICE_PATTERN = "([0-9]{1,9})(,[0-9]{2}) ?€";
-    public static final String AGE_PATTERN = "([1][8-9])|([2][0-9]|[3][0-9])|([4][0-9])|([5][0-9])|([6][0-9])";
+    public static final String AGE_PATTERN = "(1[8-9])|(2[0-9]|3[0-9])|(4[0-9])|(5[0-9])|(6[0-9])";
 
 
     // Local variablen
@@ -26,9 +26,13 @@ public class Eventcard {
 
     // Constructor
     public Eventcard(String name, String date, String time, String price, String age, double rating){
+        this(price, age, rating);
         setName(name);
         setDate(date);
         setTime(time);
+    }
+
+    private Eventcard(String price, String age, double rating){
         setPrice(price);
         setAge(age);
         setRating(rating);
@@ -70,7 +74,7 @@ public class Eventcard {
             this.time = time;
         }
         else {
-            throw new NumberFormatException("ERROR: incorrect time entry (hh:mm)");
+            throw new NumberFormatException("ERROR: incorrect time entry (hh.mm)");
         }
     }
 
@@ -115,7 +119,7 @@ public class Eventcard {
 
     public String outputEventCard(){
         return String.format(
-                "\nEvent: %s\nDate: %s\nTime: %s\nPrice: %s\nRating: %.1f\nAge: %s\n",
+                "\nEvent: %s\nDate: %s\nTime: %s pm\nPrice: %s\nRating: %.1f\nAge: %s\n",
                 getName(), getDate(), getTime(), getPrice(), getRating(), getAge());
     }
 }
--- src/aufgaben/aufgaben_wiederholungen/eventCards/Main.java
+++ src/aufgaben/aufgaben_wiederholungen/eventCards/Main.java
@@ -12,9 +12,12 @@ public class Main {
 
         Eventcard[] cards = new Eventcard[5];
 
+        // <=line=>
+        System.out.print("===========================================");
+
         // Open Air Dance Night (Event)
         cards[0] = new Eventcard(
-                "Open Air Dance Night", "28.08.2024", "06:00", "20,00 €", "24", 5.0);
+                "Open Air Dance Night", "28.08.2024", "06.00", "20,00 €", "18", 5.0);
         System.out.print(cards[0].outputEventCard());
 
         // <=line=>
@@ -24,6 +27,16 @@ public class Main {
         cards[1] = new Eventcard(
                 "Trash", "28.08.2024", "06:00", "50,00 €", "18",1.0);
         System.out.print(cards[1].outputEventCard());
+
+        // <=line=>
+        System.out.print("===========================================");
+
+        // Summer Love (Event)
+        cards[2] = new Eventcard(
+                "Summer Love", "09.09.2024","11.00", "100,00 €", "18", 4.9);
+        System.out.print(cards[2].outputEventCard());
+
+        // <=line=>
+        System.out.print("===========================================");
     }
 }
-
